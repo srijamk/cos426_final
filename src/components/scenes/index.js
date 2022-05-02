daa5f28598f5d7b253ce427021483d0ce4f11f7e
Srija <srijamk@gmail.com>
2022-05-02 14:17:09 -0400

    add a choose player scene

--- src/components/scenes/index.js
+++ src/components/scenes/index.js
@@ -2,3 +2,4 @@ export { default as SeedScene } from './SeedScene.js';
 export { default as MainScene } from './MainScene.js';
 export { default as StartScene } from './StartScene.js';
 export { default as DifficultyScene } from './DifficultyScene.js';
+export { default as ChoosePlayerScene } from './ChoosePlayerScene.js';
