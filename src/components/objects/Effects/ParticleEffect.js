d27e2d56fdf343f0a5f767fa665ccdc980051a7e
Beiqi Zou <zoubeiqi@corp.netease.com>
2022-05-03 22:20:30 -0400

    add project writeup; with small modifications

--- src/components/objects/Effects/ParticleEffect.js
+++ src/components/objects/Effects/ParticleEffect.js
@@ -1,8 +1,17 @@
 import * as THREE from 'three'
 
-class ParticleEffect {
-    constructor () {
+/**
+ * status = {
+ *  parentPosition: Vec3, // includes parent position
+ *  parentVelocity, 
+ *  moveDirection, 
+ *  number of particles
+ * }
+ */
 
+class ParticleEffect {
+    constructor (status) {
+        this.position = status.parentPosition;
     }
 
     createParticles () {
